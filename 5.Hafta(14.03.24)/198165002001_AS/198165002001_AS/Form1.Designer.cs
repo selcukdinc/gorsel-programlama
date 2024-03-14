7445b72ab459a88195cfaa6771358a5dd3618ee0
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-03-14 17:50:15 +0300

    Repo redefined

---
+++ 5.Hafta(14.03.24)/198165002001_AS/198165002001_AS/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace _198165002001_AS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(29, 24);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 151);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1117, 677);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "AKES ";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ 5.Hafta(14.03.24)/198165002001_AS/198165002001_AS/Form1.cs
@@ -0,0 +1,10 @@
+namespace _198165002001_AS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
