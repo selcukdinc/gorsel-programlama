d2468dd4786bee20583b272cf497ae5c000cf32e
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-04-19 11:54:24 +0300

    10. Hafta eklendi

---
+++ 10.Hafta(18.04.24)/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace _10.Hafta_18._04._24_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusStrip2 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(13, 12);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(629, 378);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.trackBar1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(621, 352);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.progressBar1);
+            this.tabPage2.Controls.Add(this.statusStrip2);
+            this.tabPage2.Controls.Add(this.menuStrip1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(621, 352);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(81, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(250, 245);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "Sample Text";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(17, 19);
+            this.trackBar1.Maximum = 60;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 296);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(621, 352);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(621, 400);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(3, 3);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(615, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(649, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusStrip2
+            // 
+            this.statusStrip2.Location = new System.Drawing.Point(3, 327);
+            this.statusStrip2.Name = "statusStrip2";
+            this.statusStrip2.Size = new System.Drawing.Size(615, 22);
+            this.statusStrip2.TabIndex = 1;
+            this.statusStrip2.Text = "statusStrip2";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(70, 17);
+            this.toolStripStatusLabel1.Text = "Dev by Tech";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(252, 258);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(185, 30);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ 8.Hafta(18.04.24)/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8.Hafta_18._04._24_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            richTextBox1.ZoomFactor = trackBar1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 50;
+            //progressBar1.Value += 12;
+        }
+    }
+    /*
+     * 
+     *  HAFTAYA HATIRLATMA,
+     *          ANCHOR VE DOCK HATIRLATILACAK, derste dock özelliği gösterildi
+     *  
+     * rich textbox, tab control, date time picker, flow layout, tooltip
+     * rich textbox: normal textbox'tan daha çok görsel fayda sağlar.
+     *      zoom factor; yakınlaştırma
+     *      link ; güncel versiyonlarda çalışmıyor
+     *      yapılan işlemleri geriye ve sonrasında ileriye sarabiliriz
+     *      .SelectionBackColor     Seçilen metnin arkaplan rengini değiştirebiliriz    
+     *      .SelectionFont          Seçilen metnin fontunu değiştirebiliriz         
+     *      .SelectionAlignment     Seçilen metnin 
+     *      .SelectionIndent        boşluk verilir
+     *      .SelectionBullet        madde işareti verir
+     *      
+     *      .canUndo --> bool statements
+     *      .canRedo --> bool statements
+     *      
+     *      .Redo();
+     *      .Undo();
+     *      
+     *      .Selectioncharoffset    üst veya alt karakter
+     *      
+     *      Application.StartupPath;
+     *      SaveFileDialog
+     *      "Zengin Metin | *.rtf"
+     *      
+     * deteced url; .Net framework 4.8'de çalışmıyor, 4.6'ya geçildiğinde çalışabiliyor
+     *                  Project > Properties > Target Framework
+     * 
+     * 
+     * farklı butonlarla aynı metotu çalıştırabiliriz,
+     *      Button btn = (Button)sender;
+     *          şeklinde bir kullanım ile 
+     *          sender, gönderen, yönlendiren komponentin bilgileri taşır.
+     *          
+     *  
+     *  
+     *  
+     *  Tab Kontrol
+     *  tab control | tab page arasındaki fark
+     *          sekmelerle ilgili işlemler için tab control
+     *          sekme içinde bir özellikle işlemler için tab page control
+     *  
+     *  tab page control grubunda, 
+     *          radiobutonları aynı methota bağlanmış
+     *  
+     *      .SelectedIndex;
+     *      .SelecetTab(int x);
+     *      .TabControl.TabPages.Add(string x); x tab text'i olabilir // string ifade vermeden yeni tab oluşur mu?
+     *      // tabpages değişimi sırasında 0 index'inde -1 değeri alırsa ne olur?
+     *      
+     *      Event, 
+     *      SelectedIndexChanged       tablar arası geçiş olduğunda çalışan event
+     *      
+     *
+     *  Date Time Picker
+     *  combobox şeklilnde kullanılabilir
+     *  S Y N T A X
+     *      DateTime std = dtStart.Value.Data;
+     *      DateTime edt = dtEnd.Value.Data;
+     *      TimeSpan ts
+     *      
+     *      dtStart 
+     *          tarih formatları    Long, short
+     *          
+     *     Mounth Calendar
+     *     Width : sütunda kaç adet ay olsun
+     *     Height: satırda kaç adet ay olsun
+     *          Event
+     *              dateChanged
+     *     
+     *     FlowLayoutPanel
+     *          panel       -->     Container
+     *          groupBox    -->     Container
+     *     bir akış içerisinde komponenetleri sıralar, panel ve groupBox'tan farkı budur
+     *     
+     *     Dock ile panellerin konumları değiştirilebilir
+     *     
+     *     Error Provider 
+     *     .SetError(object x, )
+     *     
+     *     NotifyIcon
+     *     bildirim göndermek için kullanılır
+     *     
+     *     ToolTip
+     *     fareyi üzerinde tuttuğumuz komponent hakkında bilgi alabiliriz
+     *     
+     *     Context Menu Strip || Menu Strip
+     *          Menu Strip              formun üstünde görünür
+     *          Context Menu Strip      sağ tıklandığında açılır
+     *          
+     *      Context Menu Strip
+     *          component özelinde atama yapılabilir
+     *      
+     *      Menu Strip 
+     *          seperator eklenebilir
+     *          comboBox, textBox eklenebilir
+     *     
+     *      statusStrip
+     *          spring özelliği
+     *      
+     *      timer
+     *          tick özelliği
+     *          interval milisaiyede cinsinden tick olayını çalıştırır
+     *          .Start(); 
+     *          ile çalıştırılmalı
+     */
+}
