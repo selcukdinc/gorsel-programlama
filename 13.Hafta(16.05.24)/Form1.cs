2085ebebc0b69a9622946abdbb3a6f87d2dda3b1
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-05-16 21:25:10 +0300

    DataGridView işlendi, dataset, datatable ve veritabanından bahsedildi

---
+++ 13.Hafta(16.05.24)/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13.Hafta_16._05._24_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] harfler = { "A","B","C","D" };
+            dataGridView1.RowCount = 3;
+            dataGridView1.ColumnCount = 3;
+            //dataGridView1[1, 2].Value = "hey hey hey";
+            Random R = new Random();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1[i, j].Value = R.Next(50,101); 
+                }
+            }
+            //MessageBox.Show(
+            //[2,0].Value.ToString());
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                dataGridView1.Columns[i].HeaderText = harfler[i];
+                dataGridView1.Rows[i].HeaderCell.Value = $"{i + 1}";
+            }
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[0].Width = 150;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt1 = new DataTable("ogrenciler");
+            
+            dt1.Columns.Add("ogrenciNo", typeof(int));
+            dt1.Columns.Add("ogrenciAdi", typeof(string));
+
+            dt1.Rows.Add(123, "Ali");
+            dt1.Rows.Add(124, "Veli");
+            dt1.Rows.Add(125, "Volkan");
+
+            //dataGridView1.DataSource = dt1;
+
+
+            DataTable dt2 = new DataTable("notlar");
+
+            dt2.Columns.Add("ogrenciNo", typeof(int));
+            dt2.Columns.Add("notu", typeof(int));
+
+            dt2.Rows.Add(123, 25);
+            dt2.Rows.Add(124, 30);
+            dt2.Rows.Add(125, 35);
+
+            DataSet ds = new DataSet("Kayitlar");
+            ds.Tables.Add(dt1);
+            ds.Tables.Add(dt2);
+
+            dataGridView1.DataSource = ds.Tables[1];// indis yerine tablonun adıda kullanılabilir
+        
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show("satır no:"+e.RowIndex.ToString());
+            MessageBox.Show("sütun no:" + e.ColumnIndex.ToString());
+            MessageBox.Show("içerideki veri =" + dataGridView1[e.ColumnIndex,e.RowIndex].Value);
+        
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(dataGridView1.SelectedRows.Count.ToString());
+            for (int i = 0; i < dataGridView1.SelectedRows.Count; i++)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[i].Index);
+            }
+         
+        }
+    }
+}
---
+++ 13.Hafta(16.05.24)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13.Hafta_16._05._24_
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+        /*
+         
+            DataGridView
+                !! satır ve sütunların yeri farklı
+                
+            ödev
+                Excel benzeri bir uygulama
+                - A-Z otomatik devamı gelecek 
+            
+        
+            seçilen satır ve sütunu silme
+         
+            
+         */
+    }
+}
