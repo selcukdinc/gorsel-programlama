b496df5a674bc38efd9e9f1f0dab92064a8f68a1
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-05-25 10:16:08 +0300

    Dosya oluşturma ve varığının kontrolü

---
+++ Lab-13 (23.05.24)/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Lab_13__23._05._24_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.btnDosyaDurum = new System.Windows.Forms.Button();
+            this.tbDosyaAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbUzanti = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Location = new System.Drawing.Point(95, 167);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(101, 23);
+            this.btnOlustur.TabIndex = 0;
+            this.btnOlustur.Text = "Dosya Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // btnDosyaDurum
+            // 
+            this.btnDosyaDurum.Location = new System.Drawing.Point(279, 167);
+            this.btnDosyaDurum.Name = "btnDosyaDurum";
+            this.btnDosyaDurum.Size = new System.Drawing.Size(101, 23);
+            this.btnDosyaDurum.TabIndex = 1;
+            this.btnDosyaDurum.Text = "Dosyayı kontrol et";
+            this.btnDosyaDurum.UseVisualStyleBackColor = true;
+            this.btnDosyaDurum.Click += new System.EventHandler(this.btnDosyaDurum_Click);
+            // 
+            // tbDosyaAdi
+            // 
+            this.tbDosyaAdi.Location = new System.Drawing.Point(158, 101);
+            this.tbDosyaAdi.Name = "tbDosyaAdi";
+            this.tbDosyaAdi.Size = new System.Drawing.Size(100, 20);
+            this.tbDosyaAdi.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dosya Adı: ";
+            // 
+            // cbUzanti
+            // 
+            this.cbUzanti.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbUzanti.FormattingEnabled = true;
+            this.cbUzanti.Items.AddRange(new object[] {
+            ".ini",
+            ".txt"});
+            this.cbUzanti.Location = new System.Drawing.Point(279, 101);
+            this.cbUzanti.Name = "cbUzanti";
+            this.cbUzanti.Size = new System.Drawing.Size(93, 21);
+            this.cbUzanti.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 316);
+            this.Controls.Add(this.cbUzanti);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDosyaAdi);
+            this.Controls.Add(this.btnDosyaDurum);
+            this.Controls.Add(this.btnOlustur);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.Button btnDosyaDurum;
+        private System.Windows.Forms.TextBox tbDosyaAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbUzanti;
+    }
+}
+
---
+++ Lab-13 (23.05.24)/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab_13__23._05._24_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbUzanti.SelectedIndex = 0;
+        }
+
+        private string dosyaIsmi()
+        {
+            return tbDosyaAdi.Text + cbUzanti.Text;
+        }
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(dosyaIsmi()))
+                {
+                    File.Create(dosyaIsmi());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void btnDosyaDurum_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(dosyaIsmi()))
+                {
+                    MessageBox.Show("Dosya bulunmaktadır");
+                }
+                else
+                {
+                    MessageBox.Show("Dosya dizinde bulunmuyor");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+    }
+}
