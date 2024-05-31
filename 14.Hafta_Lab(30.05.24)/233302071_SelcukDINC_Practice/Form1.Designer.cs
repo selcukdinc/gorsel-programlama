33f3d1f73b3e59f8d56bccc338c0ca627e1f916b
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-05-31 09:30:34 +0300

    14. Hafta, pratik uygulaması

---
+++ 14.Hafta_Lab(30.05.24)/233302071_SelcukDINC_Practice/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace _233302071_SelcukDINC_Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbAd = new System.Windows.Forms.TextBox();
+            this.tbOgNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbEposta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(277, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(491, 310);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(65, 144);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // tbAd
+            // 
+            this.tbAd.Location = new System.Drawing.Point(94, 36);
+            this.tbAd.Name = "tbAd";
+            this.tbAd.Size = new System.Drawing.Size(153, 20);
+            this.tbAd.TabIndex = 3;
+            // 
+            // tbOgNo
+            // 
+            this.tbOgNo.Location = new System.Drawing.Point(94, 62);
+            this.tbOgNo.Name = "tbOgNo";
+            this.tbOgNo.Size = new System.Drawing.Size(153, 20);
+            this.tbOgNo.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Öğrenci No";
+            // 
+            // tbEposta
+            // 
+            this.tbEposta.Location = new System.Drawing.Point(94, 88);
+            this.tbEposta.Name = "tbEposta";
+            this.tbEposta.Size = new System.Drawing.Size(153, 20);
+            this.tbEposta.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "E-Posta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 347);
+            this.Controls.Add(this.tbEposta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbOgNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbAd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbAd;
+        private System.Windows.Forms.TextBox tbOgNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbEposta;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 14.Hafta_Lab(30.05.24)/233302071_SelcukDINC_Practice/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace _233302071_SelcukDINC_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            veriGoster();
+        }
+
+        static string _conString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+ Application.StartupPath +@"\dbStudents.accdb";
+
+        private void veriYaz()
+        {
+            try
+            {
+                using (OleDbConnection bag = new OleDbConnection(_conString))
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    cmd.Connection = bag;
+                    cmd.CommandText = "INSERT INTO tblStudents (ogrAd, ogrNo, ogrEposta) VALUES (P1, P2, P3)";
+                    cmd.Parameters.Add("@P1", OleDbType.VarChar).Value = tbAd.Text;
+                    cmd.Parameters.Add("@P2", OleDbType.SmallInt).Value = Convert.ToInt32(tbOgNo.Text);
+                    cmd.Parameters.Add("@P3", OleDbType.VarChar).Value = tbEposta.Text;
+                    bag.Open();
+                    cmd.ExecuteNonQuery();
+                    bag.Close();
+                }
+                tbAd.Text = "";
+                tbOgNo.Text = "";
+                tbEposta.Text = "";
+                veriGoster();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void veriGoster()
+        {
+            try
+            {
+                using (OleDbConnection bag = new OleDbConnection(_conString))
+                using (OleDbCommand cmd1 = new OleDbCommand())
+                {
+                    cmd1.Connection = bag;
+                    cmd1.CommandText = "SELECT * FROM tblStudents";
+                    DataTable dt = new DataTable();
+                    bag.Open();
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd1);
+                    da.Fill(dt);
+                    bag.Close();
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (tbAd.Text != "" && tbOgNo.Text != "" && tbEposta.Text != "")
+            {
+                veriYaz();
+                veriGoster();
+            }
+            else MessageBox.Show("Boş Alan Bırakmayın");
+        }
+
+      
+    }
+}
